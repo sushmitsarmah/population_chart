dc3e87010d8d22968b3e13668af5f85010d9c091
sushmit sarmah <sushmitsarmah@gmail.com>
2019-06-18 00:09:22 +0530

    added heroku postgres url

--- import_data.py
+++ import_data.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
-POSTGRESQL_URL = 'postgresql://postgres:postgres@localhost/mariam'
+# POSTGRESQL_URL = 'postgresql://postgres:postgres@localhost/mariam'
+POSTGRESQL_URL = 'postgres://cpejdskelcflry:6b5a08674306200dfd12ea50e3da1c0607de44d91b9fd15f20a04ca4e197ba53@ec2-184-73-169-163.compute-1.amazonaws.com:5432/ddrkkh6i24bl0r'
 engine = create_engine(POSTGRESQL_URL)
 
 df = pd.read_csv('public/data/population_data.csv', encoding='latin1')
